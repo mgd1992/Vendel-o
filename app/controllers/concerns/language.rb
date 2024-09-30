19421bebe1597cd0be08887a5fb4cf70eb8d390d
marco garbini <marcoogarbini@gmail.com>
2024-09-30 17:56:26 +0200

    giving styles to landing page(header, filters, and products)

--- app/controllers/concerns/language.rb
+++ app/controllers/concerns/language.rb
@@ -4,15 +4,15 @@ module Language
   included do
     around_action :switch_locale
 
-
     private
 
     def switch_locale(&action)
       I18n.with_locale(locale_from_header, &action)
     end
-    
+
     def locale_from_header
       request.env['HTTP_ACCEPT_LANGUAGE']&.scan(/^[a-z]{2}/)&.first
+      I18n.available_locales.map(&:to_s).include?(locale) ? locale : I18n.default_locale
     end
   end
 end
